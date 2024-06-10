6c786d2ba3e070186e432d79f81738d4db5a2a00
osy <osy9757@gmail.com>
2024-06-11 04:17:56 +0900

    Feat : aes encrypt, decrypt 구현

--- include/aes_crypt.h
+++ include/aes_crypt.h
@@ -5,6 +5,7 @@
 
 class AESCrypt {
 public:
+    // AES 암호화 함수
     static std::string encrypt(const std::string &plaintext, const std::string &key, 
         const std::string &iv, int key_length);
     static std::string decrypt(const std::string &ciphertext, const std::string &key, 
